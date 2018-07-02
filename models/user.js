75b2734540d8cfa19fed21da5e0de956d1d8829e
jake <newlee0827@gmail.com>
2018-07-02 12:40:02 -0700

    Backend for the login session
    has base functions for the login

---
+++ models/user.js
@@ -0,0 +1,113 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+const jwt = require("jsonwebtoken");
+const _ = require("lodash");
+const bcrypt = require('bcryptjs');
+
+// SECRET KEY for jwt token
+const SECRET_KEY = "zotzotzot";
+
+mongoose.connect('mongodb://localhost/ExampleZotCoin');
+
+var _isUCIEmail = function(address){
+    if (! validator.isEmail(address)){
+        return false;
+    }
+    var dns = address.split('@');
+ 
+    if (!(dns[1] === 'uci.edu')){
+        return false;
+    }
+    return true;
+};
+
+var UserSchema = new mongoose.Schema({
+    email: {
+        type:String,
+        required: true,
+        trim: true,
+        minlength:1,
+        unique: true, 
+        validate:{
+            validator: (value) => {
+                return _isUCIEmail(value);
+            },
+            message: "{VALUE} is not valid email address"
+        }
+    },
+
+    password:{
+        type:String,
+        require:true,
+        minlength: 8
+    },
+
+    tokens:[{
+        access: {
+            type:String,
+            required:true
+        },
+        token:{
+            type:String,
+            required: true
+        }
+    }]
+});
+
+UserSchema.methods.toJSON = function(){
+    var user = this;
+    var userObject = user.toObject();
+
+    return _.pick(userObject, ['_id', 'email']);
+};
+
+UserSchema.methods.generateAuthToken = function(){
+    var user = this;
+    var access = 'auth';
+    var token = jwt.sign({_id: user._id.toHexString(), access}, 'zotzotzot').toString();
+
+    //user.tokens.push({access, token});
+    user.tokens = user.tokens.concat([{access, token}]);
+    return user.save().then(()=>{
+        return token;
+    });
+};
+
+UserSchema.statics.findByToken = function(token){
+    var User = this;
+    var decoded;
+
+    try{
+        decoded = jwt.verify(token, SECRET_KEY);
+    }catch(e){
+        return new Promise((resolve, reject) => {
+            reject();
+        });
+    }
+
+    return User.findOne({
+        _id:decoded._id,
+        'tokens.token':token,
+        'tokens.access': 'auth'
+    })
+};
+
+
+UserSchema.pre('save', function(next){
+    var user = this;
+
+    if(user.isModified('password')){
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(user.password, salt, (err, hash) => {
+                user.password = hash;
+                next();
+            });
+        });
+    }else{
+        next();
+    }
+});
+
+var User = mongoose.model('User', UserSchema);
+
+module.exports = {User};
